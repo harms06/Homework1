56c04ed668e3ddd39ac7fe5a1b65886859602728
harms06 <harms06@hotmail.com>
2019-04-08 22:31:23 +0300

    homework2

---
+++ src/MathUtilsInt.java
@@ -0,0 +1,13 @@
+
+public interface MathUtilsInt {
+     public int max(int x,int y);
+    public double max(double x,double y);
+    public byte max(byte x,byte y);
+    public short max(short x,short y);
+    public float max(float x,float y);
+    public long max(long x,long y);
+   public int median(int[] y); 
+   public double average(int[] y); 
+   public int [] sum(int[] y,int [] x);
+    
+}
